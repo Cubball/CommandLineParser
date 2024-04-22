ae98d87d5f437f322fdc872d62175847716e93b0
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-22 12:38:43 +0300

    Implement general logic for parsing

--- src/CommandLineParser/Parsing/Parser.cs
+++ src/CommandLineParser/Parsing/Parser.cs
@@ -4,13 +4,43 @@ namespace CommandLineParser.Parsing;
 
 internal class Parser
 {
+    private const string FullNameOptionPrefix = "--";
+    // TODO: extract these into config class?
+    // also, should this be a string?
+    private const string ShortNameOptionPrefix = "-";
+
     private CommandDescriptor _currentCommand;
+    private bool _parseOptions = true;
+    private int _currnetPositionalArgumentIndex;
 
     public Parser(CommandDescriptor rootCommand) => _currentCommand = rootCommand;
 
     public void Parse(string[] args)
     {
         var spanOfArgs = ParseSubcommands(args);
+        while (!spanOfArgs.IsEmpty)
+        {
+            if (!_parseOptions)
+            {
+                spanOfArgs = ParsePositionalArgument(spanOfArgs);
+                continue;
+            }
+
+            var arg = spanOfArgs[0];
+            if (IsFullNameOption(arg))
+            {
+                spanOfArgs = ParseFullNameOption(spanOfArgs);
+                continue;
+            }
+
+            if (IsShortNameOptions(arg))
+            {
+                spanOfArgs = ParseShortNameOptions(spanOfArgs);
+                continue;
+            }
+
+            spanOfArgs = ParsePositionalArgument(spanOfArgs);
+        }
     }
 
     private Span<string> ParseSubcommands(Span<string> args)
@@ -47,4 +77,9 @@ internal class Parser
         // TODO:
         return args;
     }
+
+    // NOTE: should these be extracted?
+    private static bool IsFullNameOption(string arg) => arg.StartsWith(FullNameOptionPrefix, StringComparison.InvariantCulture);
+
+    private static bool IsShortNameOptions(string arg) => arg.StartsWith(ShortNameOptionPrefix, StringComparison.InvariantCulture);
 }
