1225f41354a048fe570337b9ab1e46b33610b740
Cubball <138378823+Cubball@users.noreply.github.com>
2024-06-21 19:26:23 +0300

    Add the rest of tests for ParsingResultBuilder

--- tests/CommandLineParser.Tests/Parsing/ParsingResultBuilderTests.cs
+++ tests/CommandLineParser.Tests/Parsing/ParsingResultBuilderTests.cs
@@ -1,5 +1,6 @@
 using CommandLineParser.Models;
 using CommandLineParser.Parsing;
+using CommandLineParser.Parsing.Models;
 
 using FluentAssertions;
 
@@ -91,6 +92,56 @@ public class ParsingResultBuilderTests
     }
 
     [Fact]
+    public void Build_ShouldReturnAddedOption_WhenItHasMultipleValues()
+    {
+        // Arrange
+        var command = new CommandDescriptor("main", "description", [], [],
+        [
+            new OptionDescriptor("--opt1", "description", argument: new ArgumentDescriptor<int>("opt1arg", repeated: true))
+        ]);
+        _sut.SetCurrentCommand(command);
+
+        // Act
+        _sut.AddParsedArgumentValue(command.Options[0].Argument!, 69);
+        _sut.AddParsedArgumentValue(command.Options[0].Argument!, 420);
+        var result = _sut.Build();
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        var successContext = result.SuccessContext;
+        successContext!.ParsedOptions.Should().Contain(kvp =>
+            kvp.Key == command.Options[0]
+            && kvp.Value.Count == 2
+            && kvp.Value.Contains(69)
+            && kvp.Value.Contains(420));
+    }
+
+    // NOTE: should this behavior be configurable?
+    [Fact]
+    public void Build_ShouldReturnAddedOptionsLastValue_WhenItHasOneValueButMultipleWereProvided()
+    {
+        // Arrange
+        var command = new CommandDescriptor("main", "description", [], [],
+        [
+            new OptionDescriptor("--opt1", "description", argument: new ArgumentDescriptor<int>("opt1arg"))
+        ]);
+        _sut.SetCurrentCommand(command);
+
+        // Act
+        _sut.AddParsedArgumentValue(command.Options[0].Argument!, 69);
+        _sut.AddParsedArgumentValue(command.Options[0].Argument!, 420);
+        var result = _sut.Build();
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        var successContext = result.SuccessContext;
+        successContext!.ParsedOptions.Should().Contain(kvp =>
+            kvp.Key == command.Options[0]
+            && kvp.Value.Count == 1
+            && kvp.Value.Contains(420));
+    }
+
+    [Fact]
     public void Build_ShouldReturnAddedPositionalArguments_WhenThereAreNoErrors()
     {
         // Arrange
@@ -146,7 +197,7 @@ public class ParsingResultBuilderTests
 
     // NOTE: should this behavior be configurable?
     [Fact]
-    public void Build_ShouldReturnAddedPositionalArgumentsLastValues_WhenItHasOneValueButMultipleWereProvided()
+    public void Build_ShouldReturnAddedPositionalArgumentsLastValue_WhenItHasOneValueButMultipleWereProvided()
     {
         // Arrange
         var command = new CommandDescriptor("main", "description", [],
@@ -167,5 +218,82 @@ public class ParsingResultBuilderTests
             kvp.Key == command.Arguments[0]
             && kvp.Value.Count == 1
             && kvp.Value.Contains(420));
+    }
+
+    [Fact]
+    public void Build_ShouldReturnFailureContext_WhenErrorsWereAdded()
+    {
+        // Arrange
+        var command = new CommandDescriptor("main", "description", [], [], []);
+        _sut.SetCurrentCommand(command);
+        var error = new ParsingError(ParsingErrorType.UnknownToken);
+
+        // Act
+        _sut.AddError(error);
+        var result = _sut.Build();
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        var failureContext = result.FailureContext;
+        failureContext.Should().NotBeNull();
+        failureContext!.Command.Should().Be(command);
+        failureContext.Errors.Should().HaveCount(1);
+        failureContext.Errors[0].Should().Be(error);
+    }
+
+    [Fact]
+    public void Build_ShouldReturnFailureContext_WhenPositionalArgumentsAreMissing()
+    {
+        // Arrange
+        var command = new CommandDescriptor("main", "description", [],
+        [
+            new ArgumentDescriptor<int>("arg1"),
+        ], []);
+        _sut.SetCurrentCommand(command);
+
+        // Act
+        var result = _sut.Build();
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        var failureContext = result.FailureContext;
+        failureContext.Should().NotBeNull();
+        failureContext!.Command.Should().Be(command);
+        failureContext.Errors.Should().Satisfy(e =>
+            e.Type == ParsingErrorType.MissingPositionalArgument && e.TokenName == command.Arguments[0].Name);
+    }
+
+    [Fact]
+    public void Build_ShouldReturnFailureContext_WhenRequiredOptionsAreMissing()
+    {
+        // Arrange
+        var command = new CommandDescriptor("main", "description", [], [],
+        [
+            new OptionDescriptor("--opt1", "description", argument: new ArgumentDescriptor<int>("arg1"), required: true)
+        ]);
+        _sut.SetCurrentCommand(command);
+
+        // Act
+        var result = _sut.Build();
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        var failureContext = result.FailureContext;
+        failureContext.Should().NotBeNull();
+        failureContext!.Command.Should().Be(command);
+        failureContext.Errors.Should().Satisfy(e =>
+            e.Type == ParsingErrorType.MissingRequiredOption && e.TokenName == command.Options[0].FullName);
+    }
+
+    [Fact]
+    public void AddParsedArgumentValue_ShouldThrowInvalidOperationException_WhenCurrentCommandWasNotSet()
+    {
+        // Arrange
+        var argument = new ArgumentDescriptor<int>("arg1");
+        var action = () => _sut.AddParsedArgumentValue(argument, 69);
+
+        // Act, Assert
+        action.Should().Throw<InvalidOperationException>()
+            .WithMessage("The current command needs to be set before performing this action");
     }
 }
